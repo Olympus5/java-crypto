eef1a4c47318927fdb74777776ae30738518e6e5
olympus5 <erwan.iquel@gmail.com>
2023-02-27 16:36:16 +0100

    cipher

--- src/test/java/fr/olympus5/learning/JceTest.java
+++ src/test/java/fr/olympus5/learning/JceTest.java
@@ -3,15 +3,14 @@ package fr.olympus5.learning;
 import fr.olympus5.helper.ConverterHelper;
 import org.junit.jupiter.api.Test;
 
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
+import javax.crypto.*;
 import javax.crypto.spec.DESKeySpec;
 import javax.crypto.spec.DESedeKeySpec;
 import javax.crypto.spec.PBEKeySpec;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
+import javax.crypto.spec.PBEParameterSpec;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
 
@@ -84,4 +83,106 @@ class JceTest {
         final DESedeKeySpec keySpec = (DESedeKeySpec) keyFactory.getKeySpec(secretKey, DESedeKeySpec.class);
         System.out.println(ConverterHelper.bytesToHex(keySpec.getKey()));
     }
+
+    @Test
+    void cipherInstance() throws NoSuchPaddingException, NoSuchAlgorithmException {
+        // transformation = algorithm/mode/padding or algorithm (with default mode and padding)
+        final Cipher cipher = Cipher.getInstance("DES/CBC/NoPadding");
+        final Cipher cipher2 = Cipher.getInstance("AES");
+    }
+
+    @Test
+    void cipher() throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        final KeyGenerator keyGenerator = KeyGenerator.getInstance("DES");
+        keyGenerator.init(56);
+        final SecretKey secretKey = keyGenerator.generateKey();
+        System.out.printf("cle(%s,%s): %s%n",
+                secretKey.getAlgorithm(),
+                secretKey.getFormat(),
+                ConverterHelper.bytesToHex(secretKey.getEncoded()));
+
+        final Cipher cipher = Cipher.getInstance("DES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        final byte[] encryptedMessage = cipher.doFinal("Hello world!".getBytes(StandardCharsets.US_ASCII));
+        System.out.println("encrypted text: " + ConverterHelper.bytesToHex(encryptedMessage));
+
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        final byte[] decryptedMessage = cipher.doFinal(encryptedMessage);
+        System.out.println("decrypted text: " + new String(decryptedMessage, StandardCharsets.US_ASCII));
+    }
+
+    @Test
+    void cipherKey() throws NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, InvalidKeyException {
+        final KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(256);
+        final SecretKey secretKey = keyGenerator.generateKey();
+        System.out.println("key: " + ConverterHelper.bytesToHex(secretKey.getEncoded()));
+
+        keyGenerator.init(128);
+        final SecretKey encryptionKey = keyGenerator.generateKey();
+        final Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding");
+        cipher.init(Cipher.WRAP_MODE, encryptionKey);
+        final byte[] encryptedKeyBytes = cipher.wrap(secretKey);
+        System.out.println("encrypted key: " + ConverterHelper.bytesToHex(encryptedKeyBytes));
+
+        cipher.init(Cipher.UNWRAP_MODE, encryptionKey);
+        final Key decryptedKey = cipher.unwrap(encryptedKeyBytes, "AES", Cipher.SECRET_KEY);
+        System.out.println("decrypted key: " + ConverterHelper.bytesToHex(decryptedKey.getEncoded()));
+    }
+
+    @Test
+    void keyAlgorithmParameterSpec() throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException, IOException {
+        final String transformation = "PBEWithMD5AndDES";
+        final SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance(transformation);
+        final PBEKeySpec keySpec = new PBEKeySpec("pass".toCharArray());
+        final SecretKey secretKey = secretKeyFactory.generateSecret(keySpec);
+        final PBEParameterSpec algorithmParameterSpec = new PBEParameterSpec(
+                new SecureRandom().generateSeed(8), 1000);
+
+        final Cipher cipher = Cipher.getInstance(transformation);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, algorithmParameterSpec);
+        final byte[] encryptedTextBytes = cipher.doFinal("Hello world!".getBytes());
+        System.out.println("encrypted text: " + ConverterHelper.bytesToHex(encryptedTextBytes));
+
+        final AlgorithmParameters algorithmParameters = AlgorithmParameters.getInstance(transformation);
+        algorithmParameters.init(cipher.getParameters().getEncoded());
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, algorithmParameters);
+        System.out.println("decrypted text: " + new String(cipher.doFinal(encryptedTextBytes)));
+    }
+
+    @Test
+    void desCipher() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
+        final KeyGenerator keyGenerator = KeyGenerator.getInstance("DES");
+        final SecretKey secretKey = keyGenerator.generateKey();
+        String messageToEncrypt = "Hello World!";
+
+        encryptDecrypt(secretKey, "DES", messageToEncrypt);
+        encryptDecrypt(secretKey, "DES/ECB/PKCS5Padding", messageToEncrypt);
+        encryptDecrypt(secretKey, "DES/CBC/PKCS5Padding", messageToEncrypt);
+        encryptDecrypt(secretKey, "DES/PCBC/PKCS5Padding", messageToEncrypt);
+        encryptDecrypt(secretKey, "DES/CFB/PKCS5Padding", messageToEncrypt);
+        encryptDecrypt(secretKey, "DES/OFB/PKCS5Padding", messageToEncrypt);
+    }
+
+    @Test
+    void aesCipher() throws NoSuchAlgorithmException, InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
+        final KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(256, SecureRandom.getInstance("SHA1PRNG"));
+        final SecretKey secretKey = keyGenerator.generateKey();
+
+        encryptDecrypt(secretKey, "AES", "Hello World!");
+    }
+
+    private static void encryptDecrypt(final SecretKey secretKey, final String transformation, final String messageToEncrypt) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
+        final Cipher desCipher = Cipher.getInstance(transformation);
+        desCipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        final byte[] encryptedTextBytes = desCipher.doFinal(messageToEncrypt.getBytes());
+        System.out.println(ConverterHelper.bytesToHex(encryptedTextBytes));
+
+        final Cipher desDecipher = Cipher.getInstance(transformation);
+        desDecipher.init(Cipher.DECRYPT_MODE, secretKey, desCipher.getParameters());
+        final byte[] decryptedTextBytes = desDecipher.doFinal(encryptedTextBytes);
+        System.out.println(new String(decryptedTextBytes));
+    }
+
 }
