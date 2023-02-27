9f45698b23bf4885689be400ee772373dcc4496e
olympus5 <erwan.iquel@gmail.com>
2023-02-27 12:08:22 +0100

    secret key factory

--- src/test/java/fr/olympus5/learning/JceTest.java
+++ src/test/java/fr/olympus5/learning/JceTest.java
@@ -5,8 +5,15 @@ import org.junit.jupiter.api.Test;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+import javax.crypto.spec.DESedeKeySpec;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 
 class JceTest {
     @Test
@@ -51,5 +58,30 @@ class JceTest {
                 secretKey.getAlgorithm(),
                 secretKey.getFormat(),
                 ConverterHelper.bytesToHex(secretKey.getEncoded()));
+    }
+
+    @Test
+    void keySpecToSecretKey() throws NoSuchAlgorithmException, InvalidKeyException, InvalidKeySpecException {
+        final KeyGenerator keyGenerator = KeyGenerator.getInstance("DES");
+        byte[] secretKeyBytes = keyGenerator.generateKey().getEncoded();
+        final KeySpec secretKeySpec = new DESKeySpec(secretKeyBytes);
+        final SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("DES");
+        final SecretKey secretKey = secretKeyFactory.generateSecret(secretKeySpec);
+
+        System.out.printf("cle(%s,%s): %s%n",
+                secretKey.getAlgorithm(),
+                secretKey.getFormat(),
+                ConverterHelper.bytesToHex(secretKey.getEncoded()));
+    }
+
+    @Test
+    void secretKeyToKeySpec() throws NoSuchAlgorithmException, InvalidKeySpecException {
+        final KeyGenerator keyGenerator = KeyGenerator.getInstance("DESede");
+        keyGenerator.init(168, SecureRandom.getInstance("SHA1PRNG"));
+        final SecretKey secretKey = keyGenerator.generateKey();
+        final SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DESede");
+
+        final DESedeKeySpec keySpec = (DESedeKeySpec) keyFactory.getKeySpec(secretKey, DESedeKeySpec.class);
+        System.out.println(ConverterHelper.bytesToHex(keySpec.getKey()));
     }
 }
